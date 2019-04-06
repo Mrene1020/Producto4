8bacf5c41a3f037a506cecb9eed3d4d111a2f0fe
Your Name <you@example.com>
2019-04-06 00:36:05 -0400

    Entity Framework instalado

---
+++ Producto/Producto/Models/Producto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Producto.Models
+{
+    public class Producto
+    {
+        int ProductoID;
+        string Nombre;
+        Double Precio;
+    }
+}
